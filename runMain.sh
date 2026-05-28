44633b6bbbf9ba205db98bd3cb68b1300f9f276a
nadjano <nnolte@ebi.ac.uk>
2026-05-28 12:58:38 +0200

    Update .gitignore, refine output in main function, and clean up runMain.sh

--- runMain.sh
+++ runMain.sh
@@ -1,38 +1,4 @@
 
-# Desiree with desiree annotation
+# Running the main pipeline with Nextflow, using the specified parameters and configuration.
 
-nextflow run main.nf -resume -params-file params/desiree_liftoff.json -c conf/nextflow.config -profile conda --run_blast --mcscanx_path /DKED/scratch/nadjafn/MCScanX
-nextflow run main.nf -resume -params-file params/my_example.json -c conf/nextflow.config -profile conda --run_blast --mcscanx_path /DKED/scratch/nadjafn/MCScanX
-
-
-nextflow run main.nf -resume -params-file params/desiree.json -with-conda -c conf/nextflow.config -profile conda
-
-
-nextflow run main.nf -resume -params-file params/atlantic.json -with-conda -c conf/nextflow.config -profile conda
-
-# Atlantic with liftoff
-nextflow run main.nf -resume -params-file params/atlantic_with_liftoff.json -c nextflow.config -profile conda --run_blast --mcscanx_path /DKED/scratch/nadjafn/MCScanX
-
-
-
-# Desiree with liftoff and Bambu
-nextflow run main.nf -resume -params-file params/desiree_liftoff_bambu.json -c conf/nextflow.config -profile conda --run_blast --mcscanx_path /DKED/scratch/nadjafn/MCScanX
-
-
-# SWEEEET
-nextflow run main.nf -resume -params-file params/sweetpotato.json -c conf/nextflow.config -profile conda
-
-# wheat
-nextflow run main.nf -resume -params-file params/wheatAK58.json -c conf/nextflow.config -profile conda --run_blast
-
-
-# rice
-nextflow run main.nf -resume -params-file params/rice_Nip.json  -profile singularity --run_blast --mcscanx_path /DKED/scratch/nadjafn/MCScanX
-
-
-
-
-nextflow run main.nf -resume -params-file params/my_example.json -c conf/nextflow.config -profile conda --run_blast --mcscanx_path /DKED/scratch/nadjafn/MCScanX
-
-
-nextflow run main.nf -resume -params-file params/my_example.json  -profile singularity --run_blast 
+nextflow run main.nf -resume -params-file params/my_example.json -c conf/nextflow.config -profile singularity --run_blast --mcscanx_path /MCScanX
